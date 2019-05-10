e08517b25e883779daacee886ade956b4ca44b9c
martin.van.den.kerkhoff <martin.van.den.kerkhoff@technolution.nl>
2019-05-10 10:38:44 +0200

    Created container for return value ApxPrice

---
+++ apxprices/src/main/java/nl/technolution/apxprices/api/ApxPrice.java
@@ -0,0 +1,37 @@
+/*
+ (C) COPYRIGHT TECHNOLUTION BV, GOUDA NL
+| =======          I                   ==          I    =
+|    I             I                    I          I
+|    I   ===   === I ===  I ===   ===   I  I    I ====  I   ===  I ===
+|    I  /   \ I    I/   I I/   I I   I  I  I    I  I    I  I   I I/   I
+|    I  ===== I    I    I I    I I   I  I  I    I  I    I  I   I I    I
+|    I  \     I    I    I I    I I   I  I  I   /I  \    I  I   I I    I
+|    I   ===   === I    I I    I  ===  ===  === I   ==  I   ===  I    I
+|                 +---------------------------------------------------+
++----+            |  +++++++++++++++++++++++++++++++++++++++++++++++++|
+     |            |             ++++++++++++++++++++++++++++++++++++++|
+     +------------+                          +++++++++++++++++++++++++|
+                                                        ++++++++++++++|
+                                                                 +++++|
+ */
+package nl.technolution.apxprices.api;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import nl.technolution.IJsonnable;
+
+/**
+ * Container for APX price in euro per kwh
+ */
+public class ApxPrice implements IJsonnable {
+
+    /** euro per kwh */
+    @JsonProperty("price")
+    private final double price;
+
+    @JsonCreator
+    public ApxPrice(@JsonProperty("price") double price)    {
+        this.price = price;
+    }
+}
