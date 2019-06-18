6b26578a0eda59a1a2f56f7994df0efa99d9f9b3
Wilfred Hoogerbrugge <wilfred.hoogerbrugge@technolution.nl>
2019-06-18 12:06:50 +0200

    Removed TODO (won't DO, made NOTE permanent)

--- sunny/src/main/java/nl/technolution/sunny/solaredgemonitoring/model/Value.java
+++ sunny/src/main/java/nl/technolution/sunny/solaredgemonitoring/model/Value.java
@@ -39,10 +39,8 @@ public class Value {
     }
 
     @JsonProperty("date")
-    // NOTE WHO: The format of the string is like '2019-05-11 01:00:00' which is not a valid ISO dateTime string. So a
+    // NOTE: The format of the string is like '2019-05-11 01:00:00' which is not a valid ISO dateTime string. So a
     // custom deserializer is used.
-    // TODO WHO: Check with Martin if there is a way to specify the format here (like this
-    // http://www.adam-bien.com/roller/abien/entry/serializing_and_deserializing_a_pojo
     @JsonDeserialize(using = CustomDateTimeDeserializer.class)
     public void setDate(LocalDateTime date) {
         this.date = date;
