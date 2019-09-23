f96aeb2808b0a6d18df6ef8bd2dca7d21d4c3df6
martin.van.den.kerkhoff <martin.van.den.kerkhoff@technolution.nl>
2019-09-23 09:32:13 +0200

    Add device state batty to cacher

--- batty/src/main/java/nl/technolution/batty/xstorage/cache/MachineDataCache.java
+++ batty/src/main/java/nl/technolution/batty/xstorage/cache/MachineDataCache.java
@@ -16,18 +16,27 @@
  */
 package nl.technolution.batty.xstorage.cache;
 
+import java.util.EnumSet;
+import java.util.Set;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
 import nl.technolution.batty.app.BattyConfig;
 import nl.technolution.batty.xstorage.connection.IXStorageConnection;
 import nl.technolution.batty.xstorage.connection.IXStorageFactory;
 import nl.technolution.batty.xstorage.connection.XStorageException;
+import nl.technolution.batty.xstorage.types.EModeByte1;
 import nl.technolution.batty.xstorage.types.MachineData;
 import nl.technolution.dropwizard.services.Services;
+import nl.technolution.fritzy.wallet.IFritzyApiFactory;
+import nl.technolution.fritzy.wallet.event.EventLogger;
 
 /**
  * 
  */
 public class MachineDataCache implements IMachineDataCacher {
 
+    private static final Set<EModeByte1> CHARGE_MODES = EnumSet.of(EModeByte1.CHARGE, EModeByte1.DISCHARGE);
     private MachineData cachedSoc;
 
     @Override
@@ -40,6 +49,7 @@ public class MachineDataCache implements IMachineDataCacher {
         IXStorageConnection connection = Services.get(IXStorageFactory.class).getConnection();
         try {
             cachedSoc = connection.getMachineData();
+            logState(cachedSoc);
         } catch (XStorageException ex) {
             throw new IllegalStateException("Unable to retrieve state of charge", ex);
         }
@@ -51,5 +61,16 @@ public class MachineDataCache implements IMachineDataCacher {
             update();
         }
         return cachedSoc;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void logState(MachineData machineData) {
+        EventLogger logger = new EventLogger(Services.get(IFritzyApiFactory.class).build());
+        boolean isCharging = CHARGE_MODES.contains(machineData.getMode().getMode1());
+        String batteryState = machineData.getMode().getMode1().name();
+        logger.logDeviceState(
+                new ImmutablePair<String, Object>("isCharging", isCharging),
+                new ImmutablePair<String, Object>("batteryState", batteryState),
+                new ImmutablePair<String, Object>("chargeLevel", machineData.getSoc()));
     }
 }
