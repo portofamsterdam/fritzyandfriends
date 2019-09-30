9d78dc86a980a4215314cf70614c4d612f1d9167
martin.van.den.kerkhoff <martin.van.den.kerkhoff@technolution.nl>
2019-09-30 16:16:14 +0200

    remove files from unused project.

--- efi/src/main/java/nl/technolution/protocols/efi/util/AbstractCustomerEnergyManager.java
+++ efi/src/main/java/nl/technolution/protocols/efi/util/AbstractCustomerEnergyManager.java
@@ -14,44 +14,57 @@
                                                         ++++++++++++++|
                                                                  +++++|
  */
-package nl.technolution.marketnegotiator.shiftable;
+package nl.technolution.protocols.efi.util;
 
-import nl.technolution.marketnegotiator.AbstractCustomerEnergyManager;
+import org.slf4j.Logger;
+
+import nl.technolution.DeviceId;
+import nl.technolution.Log;
+import nl.technolution.protocols.efi.FlexibilityRegistration;
 import nl.technolution.protocols.efi.FlexibilityRevoke;
-import nl.technolution.protocols.efi.Instruction;
+import nl.technolution.protocols.efi.FlexibilityUpdate;
 import nl.technolution.protocols.efi.InstructionStatusUpdate;
 import nl.technolution.protocols.efi.Measurement;
-import nl.technolution.protocols.efi.ShiftableRegistration;
-import nl.technolution.protocols.efi.ShiftableUpdate;
 
 /**
- * 
+ * @param <T> FlexibilityRegistration
+ * @param <S> FlexibilityUpdate
  */
-public class ShiftableNegotiator extends AbstractCustomerEnergyManager<ShiftableRegistration, ShiftableUpdate> {
+public abstract class AbstractCustomerEnergyManager<T extends FlexibilityRegistration, S extends FlexibilityUpdate>
+        implements ICustomerEnergyManager<T, S> {
 
-    private Double temparature;
+    private final Logger log = Log.getLogger();
+
+    private T registration;
+    private DeviceId deviceId;
 
     @Override
-    public Instruction flexibilityUpdate(ShiftableUpdate update) {
-        //
-        return null;
+    public void flexibilityRegistration(T flexibilityRegistration) {
+        this.deviceId = Efi.getDeviceId(flexibilityRegistration);
+        this.registration = flexibilityRegistration;
     }
 
     @Override
     public void instructionStatusUpdate(InstructionStatusUpdate instructionStatusUpdate) {
         //
-
     }
 
     @Override
     public void flexibilityRevoke(FlexibilityRevoke revocation) {
         //
-
     }
 
     @Override
     public void measurement(Measurement measurement) {
-
+        log.debug("Received measurement but nothing is done with it: " +
+                measurement.getElectricityMeasurement().getPower());
     }
 
+    public T getRegistration() {
+        return registration;
+    }
+
+    public DeviceId getDeviceId() {
+        return deviceId;
+    }
 }
